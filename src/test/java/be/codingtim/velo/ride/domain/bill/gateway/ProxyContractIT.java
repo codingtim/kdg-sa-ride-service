d74d1f84b49e60efbd333780bfc83f087183ef75
Tim Boeckstaens <boeckstaenstim@gmail.com>
2020-05-09 06:29:40 +0200

    Move slow contract test to IT
    Small fix to disable ITs by default and enable when wanted.

--- src/test/java/be/codingtim/velo/ride/domain/bill/gateway/ProxyContractIT.java
+++ src/test/java/be/codingtim/velo/ride/domain/bill/gateway/ProxyContractIT.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProxyContractTest {
+class ProxyContractIT {
 
     private final Proxy proxy = new Proxy();
 
